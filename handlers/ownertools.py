629918621c8aca5285f05b8446c971f021cd8024
Okek <87618530+skuyykek69@users.noreply.github.com>
2021-09-06 01:25:13 +0700

    fixes

--- handlers/ownertools.py
+++ handlers/ownertools.py
@@ -1,6 +1,9 @@
 # function to remove the downloaded files
 
 import os
+from time import time
+from datetime import datetime
+from config import BOT_USERNAME
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from helpers.filters import command
@@ -8,6 +11,28 @@ from helpers.decorators import sudo_users_only, errors
 
 downloads = os.path.realpath("downloads")
 raw = os.path.realpath("raw_files")
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
 
 @Client.on_message(command(["rmd", "rmdownloads", "cleardownloads"]) & ~filters.edited)
 @errors
@@ -32,3 +57,15 @@ async def clear_raw(_, message: Message):
         await message.reply_text("✅ **Menghapus semua file raw**")
     else:
         await message.reply_text("❌ **File raw kosong, sama seperti hidupmu!**")
+
+@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+@sudo_users_only
+async def get_uptime(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "🤖 bot status:\n"
+        f"• **🚀 uptime:** `{uptime}`\n"
+        f"• **⏱️ start time:** `{START_TIME_ISO}`"
+    )
